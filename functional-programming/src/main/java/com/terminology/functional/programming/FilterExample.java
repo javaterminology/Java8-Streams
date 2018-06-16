8bcce40a30a21b3184209eee814e88849a50eb7b
javaterminology <vrajasekharreddy@gmail.com>
2018-06-16 23:30:05 +0800

    streams filter example

---
+++ functional-programming/src/main/java/com/terminology/functional/programming/FilterExample.java
@@ -0,0 +1,25 @@
+package com.terminology.functional.programming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class FilterExample {
+
+	public static void main(String args[]){
+
+		List<String> list = Arrays.asList("raja","sekhar","java","devaansh","terminology");
+		//imperative style
+		for(String name : list){
+			if(!"sekhar".equals(name)){
+				System.out.println(name);
+			}
+		}
+		//functional style
+		list.stream().filter((name)-> {
+			return !name.equals("sekhar");
+		})
+		.forEach((name)->System.out.println(name));
+	}
+}
