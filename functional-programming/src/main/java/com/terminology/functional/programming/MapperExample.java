ae87912f83a3e2dc87030707c0f07693d4f570a7
javaterminology <vrajasekharreddy@gmail.com>
2018-06-17 16:38:22 +0800

    Mapper Example

---
+++ functional-programming/src/main/java/com/terminology/functional/programming/MapperExample.java
@@ -0,0 +1,61 @@
+package com.terminology.functional.programming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import com.terminology.functional.programming.pojo.Employee;
+
+public class MapperExample {
+
+
+	private static boolean isNotSekahr(String name){
+		return !name.equals("sekhar");
+	}
+
+	public static void main(String args[]){
+
+		List<String> list = Arrays.asList("raja","sekhar","ramu","devaansh","reddy");
+
+		System.out.println("<< Imperative(traditional) Style >>");
+
+		for(String name : list){
+			if(!"sekhar".equals(name)){
+				System.out.println(name);
+			}
+		}
+		//map is used transform object one type to another type.
+		//terminal operation used to terminate the stream.
+
+		System.out.println("<< Functional Style >>");
+
+		//UsingAnonymous class implementation
+
+		list.stream().filter(MapperExample::isNotSekahr)
+		.map(new Function<String, Employee>() {
+			@Override
+			public Employee apply(String name) {
+				return new Employee(name);
+			}
+		})
+		.forEach(System.out::println);//terminal operation
+		
+		//////////////////////////////////////////////////////
+
+		List<Employee> empList = list.stream().filter(MapperExample::isNotSekahr)
+				.map(Employee::new)
+				.collect(Collectors.toList());//terminal operation
+		
+		////////////////////////////forEach////////////////////
+		empList.forEach((emp)-> {
+			System.out.println(emp);
+		}
+				);
+
+		empList.forEach(System.out::println);
+	}
+}
---
+++ functional-programming/src/main/java/com/terminology/functional/programming/pojo/Employee.java
@@ -0,0 +1,33 @@
+package com.terminology.functional.programming.pojo;
+
+public class Employee {
+	
+	private String name;
+	private Integer age=29;
+	
+	public Employee(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", age=" + age + "]";
+	}
+	
+}
