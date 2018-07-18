af6f0fd7d7d8baf6dca92c7078a3deb82c122561
Rajasekhar <39824043+javaterminology@users.noreply.github.com>
2018-07-18 18:25:18 +0800

    Test Enum Error Codes

---
+++ functional-programming/src/main/java/com/terminology/functional/programming/TestEnumErrorCodes.java
@@ -0,0 +1,35 @@
+package com.terminology.functional.programming;
+
+public class TestEnumErrorCodes {
+
+	public static void main(String[] args) {
+		TestEnumErrorCodes.forError("09");
+		TestEnumErrorCodes.forError("04");
+		TestEnumErrorCodes.forError("01");
+	}
+	public static void forError(String errorCode){
+		
+		if(ErrorCodes.getErrorDescByCode(errorCode).equals("FanError")){
+			reportFanError();
+		}else if(ErrorCodes.getErrorDescByCode(errorCode).equals("FanError")){
+			reportBeltError();
+		}else if(ErrorCodes.getErrorDescByCode(errorCode).equals("FanError")){
+			reportEngineError();
+		}else{
+			fixProblemNow();
+		}
+	}
+	private static void fixProblemNow() {
+		System.out.println("Fixing Problem");
+	}
+	private static void reportBeltError() {
+		System.out.println("Belt Error");
+	}
+	private static void reportFanError() {
+		System.out.println("Fan Error");
+	}
+	private static void reportEngineError() {
+		System.out.println("Engine Error");
+	}
+
+}
