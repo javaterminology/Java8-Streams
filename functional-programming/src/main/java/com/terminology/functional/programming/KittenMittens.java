c8f6346f653d80fab559d8663ef88e385dfefff3
Rajasekhar <39824043+javaterminology@users.noreply.github.com>
2018-07-18 23:42:40 +0800

    kitten mittens

---
+++ functional-programming/src/main/java/com/terminology/functional/programming/KittenMittens.java
@@ -0,0 +1,25 @@
+package com.terminology;
+
+public class KittenMittens {
+
+	public static void main(String[] args) {
+		
+		System.out.println(kittenMittens(5));
+		System.out.println(6%2);
+		System.out.println(6/2);
+
+	}
+	
+	public static int kittenMittens(int count){
+		int noOfMittens = 0;
+		for(int i=1;i<=count;i++){
+			if(i%2==0){
+				noOfMittens+=3;
+			}else{
+				noOfMittens+=4;
+			}
+		}
+		return noOfMittens;
+	}
+
+}
